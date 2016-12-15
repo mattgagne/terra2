3b9b2a5b0c1d359a3e9a08e9f722e37a21421896
Mathieu Gagne <Mathieu Gagne>
2016-12-14 22:38:27 -0500

    some override methods vid 10

--- app/src/main/java/com/mattg/terra/MainActivity.java
+++ app/src/main/java/com/mattg/terra/MainActivity.java
@@ -2,13 +2,73 @@ package com.mattg.terra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //launch layout activity_main
+
+    private static final String TAG = "logMsg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    // See video 10 for more override methods
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+// This is what bucky had in his tutorial
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu, this adds items to the action bar if it is present
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        //Handle action bar item clicks here
+//        //automatically handle clicks on the home/up button, so long
+//        // as you specify a parent activity in ANdroidManifest.xm
+//        int id = item.getItemId();
+//
+//        //noinspection simplified if statement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelectd(item);
+//    }
 }
 // start working on tutorial
